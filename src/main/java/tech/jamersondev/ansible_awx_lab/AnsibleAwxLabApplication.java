fccb98e38d49cd9c7b6c69b29899da6a38a920af
Jamerson Souza <jamersonsouza@outlook.com>
2026-01-19 21:50:09 -0300

    first commit lab for ansible awx

---
+++ src/main/java/tech/jamersondev/ansible_awx_lab/AnsibleAwxLabApplication.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.ansible_awx_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleAwxLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleAwxLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/jamersondev/ansible_awx_lab/AnsibleAwxLabApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.ansible_awx_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsibleAwxLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
