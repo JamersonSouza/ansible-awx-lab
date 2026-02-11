e9b790c82965f7aa3dbc5044060a12f73063759f
Jamerson Souza <jamersonsouza@outlook.com>
2026-02-10 21:13:44 -0300

    feat: ativando o openfeign para requests

--- src/main/java/tech/jamersondev/ansible_awx_lab/AnsibleAwxLabApplication.java
+++ src/main/java/tech/jamersondev/ansible_awx_lab/AnsibleAwxLabApplication.java
@@ -2,8 +2,10 @@ package tech.jamersondev.ansible_awx_lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AnsibleAwxLabApplication {
 
 	public static void main(String[] args) {
