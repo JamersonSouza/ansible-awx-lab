a71f09d97c509ff7644773a830f59b2a0dcfeb67
Jamerson Souza <jamersonsouza@outlook.com>
2026-02-12 00:06:46 -0300

    feat: criação do client awx e service para rodar o playbook

---
+++ src/main/java/tech/jamersondev/ansible_awx_lab/core/controllers/AwxController.java
@@ -0,0 +1,9 @@
+package tech.jamersondev.ansible_awx_lab.core.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ansible")
+public class AwxController {
+}
--- src/main/java/tech/jamersondev/ansible_awx_lab/core/interfaces/AwxClient.java
+++ src/main/java/tech/jamersondev/ansible_awx_lab/core/interfaces/AwxClient.java
@@ -1,15 +1,31 @@
 package tech.jamersondev.ansible_awx_lab.core.interfaces;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import tech.jamersondev.ansible_awx_lab.core.model.transport.request.InventoryRequest;
+import tech.jamersondev.ansible_awx_lab.core.model.transport.request.JobTemplateRequest;
 import tech.jamersondev.ansible_awx_lab.core.model.transport.request.ProjectRequest;
-import tech.jamersondev.ansible_awx_lab.core.model.transport.response.ProjectResponse;
+import tech.jamersondev.ansible_awx_lab.core.model.transport.response.*;
 
 @FeignClient(name = "awxClient", url = "${awx.url}")
 public interface AwxClient {
 
     @PostMapping("/api/v2/projects")
     ProjectResponse createProject(@RequestBody ProjectRequest projectRequest);
+
+    @PostMapping("/api/v2/inventories/")
+    InventoryResponse createInventory(@RequestBody InventoryRequest request);
+
+    @PostMapping("/api/v2/job_templates/")
+    JobTemplateResponse createJobTemplate(@RequestBody JobTemplateRequest request);
+
+    @PostMapping("/api/v2/job_templates/{id}/launch/")
+    JobLaunchResponse launchJob(@PathVariable Long id);
+
+    @GetMapping("/api/v2/jobs/{id}/")
+    JobStatusResponse getJobStatus(@PathVariable Long id);
 
 }
